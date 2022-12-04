b5e643a9245eebe9d187870d8671f226d468e733
AMyscich <34483197+AMyscich@users.noreply.github.com>
2022-12-03 21:17:48 -0600

    Update ProjectGUI.py

--- ProjectGUI.py
+++ ProjectGUI.py
@@ -159,35 +159,13 @@ def goToPositionFromScan(idx):
     op = (math.floor(abs(totalStride/strideLength)) - idx) * strideLength * -1
     mc.stride(op)
 def optimizeImage(fileDirectory):
-    srcnn_model = keras.models.load_model('SRCNNStars')
-
     target = cv2.imread(fileDirectory, cv2.IMREAD_COLOR)
     target = cv2.cvtColor(target, cv2.COLOR_BGR2YCrCb)
-    shape = target.shape
-
-    Y_img = cv2.resize(target[:, :, 0], (int(shape[1]), int(shape[0])), cv2.INTER_CUBIC)
-    target[:, :, 0] = Y_img
-    target = cv2.cvtColor(target, cv2.COLOR_YCrCb2BGR)
-
-    Y = np.zeros((1, target.shape[0], target.shape[1], 1), dtype=np.float32)
-
-    # Normalize
-    Y[0, :, :, 0] = Y_img.astype(np.float32) / 255.
-
-    # Predict
-    pre = srcnn_model.predict(Y, batch_size=1) * 255.
-
-    # Post process output
-    pre[pre[:] > 255] = 255
-    pre[pre[:] < 0] = 0
-    pre = pre.astype(np.uint8)
-
-    # Copy y channel back to image and convert to BGR
-    output = cv2.cvtColor(target, cv2.COLOR_BGR2YCrCb)
-    output[6: -6, 6: -6, 0] = pre[0, :, :, 0]
-    output = cv2.cvtColor(output, cv2.COLOR_YCrCb2BGR)
 
     filename = Path(fileDirectory).stem
+
+    (T, output) = cv2.threshold(target,40,255,cv2.THRESH_TOZERO)
+
     # Save image
     cv2.imwrite(os.path.join("optimized",str(filename + '_optimized.jpg')), output)
 
